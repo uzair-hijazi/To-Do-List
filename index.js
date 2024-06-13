78db8304aed97f68f6f5ef8e1f89697ea3186690
uzair-hijazi <suikh23@gmail.com>
2024-06-13 18:27:14 +0500

    Tasklistbyhijazi
    Bugs fixed

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-console.log("Hello! I am going to make TODO list");
-export {};
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todoList = [];
+let cond = true;
+while (cond) {
+    let addition = await inquirer.prompt([
+        {
+            name: "checklist",
+            type: "input",
+            message: "What do you want to add in checklist?",
+        },
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want to add more items?",
+            default: "false",
+        },
+    ]);
+    todoList.push(addition.checklist);
+    cond = addition.addmore;
+    console.log(todoList);
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log("Hello! I am going to make TODO list");
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todoList = [];
+
+let cond = true;
+
+while (cond) {
+  let addition = await inquirer.prompt([
+    {
+      name: "checklist",
+      type: "input",
+      message: "What do you want to add in checklist?",
+    },
+    {
+      name: "addmore",
+      type: "confirm",
+      message: "Do you want to add more items?",
+      default: "false",
+    },
+  ]);
+  todoList.push(addition.checklist);
+  cond = addition.addmore;
+  console.log(todoList);
+}
